968448682e6b8fa6bb9e42439108347f47b2cd9a
aunimhsn <aunim.hassan@gmail.com>
2021-02-20 18:46:19 +0100

    feat: destructuring

--- js-course/js/utils/array.js
+++ js-course/js/utils/array.js
@@ -2,6 +2,11 @@ let fruits = ['apple', 'orange', 'banana', 'strawberry'];
 
 fruits.sort(); // Sort the array alphabetically
 fruits.shift(); // Remove the first element of an array and returns it
-fruits.unshift(); // Insert new element at the start on an array
+fruits.push('pear'); // Add a new element at the end of the array
 
+// Multiple data type in the same array
+let data = [22, 'green', ['pineapple', 'yellow']];
 
+// .isArray()
+let age = 22;
+Array.isArray(age); // Return false
---
+++ js-course/js/utils/destructuring.js
@@ -0,0 +1,9 @@
+// Destructuring
+
+// Without destructuring
+const person = ['John', 'Doe'];
+const firstName = person[0];
+const lastName = person[1];
+
+// With destructuring
+const[firstName, lastName] = person;
